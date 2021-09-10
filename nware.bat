55737d28e729a0132736dca91353efc7359e44b1
enazar415 <88438697+enazar415@users.noreply.github.com>
2021-09-10 13:46:51 +0200

    Update nware.bat

--- nware.bat
+++ nware.bat
@@ -3,6 +3,10 @@ C:
 TITLE Install Nware Tools
 echo Installing Nware Tools...
 
+echo Installing Chrome..
+ChromeSetup.exe
+del ChromeSetup.exe
+
 echo Downloading geany..
 curl -LJO "https://download.geany.org/geany-1.37.1_setup.exe"
 echo Installing geany..
@@ -21,10 +25,6 @@ echo Installing ParkControl..
 parkcontrolsetup64.exe /S
 del parkcontrolsetup64.exe
 
-echo Installing Chrome..
-ChromeSetup.exe
-del ChromeSetup.exe
-
 echo Downloading CMDer..
 curl -LJO "https://github.com/cmderdev/cmder/releases/download/v1.3.18/cmder_mini.zip"
 mkdir cmder
@@ -32,5 +32,12 @@ move cmder_mini.zip cmder
 cd cmder
 tar -xf cmder_mini.zip
 del cmder_mini.zip
+cd ..
+
+echo Downloading AIO-Runtimes..
+curl -LJO "https://securedl.cdn.chip.de/downloads/6890274/aio-runtimes_v2.5.0.exe?cid=54450072&platform=chip&1622137696-1622145196-914172-B-59f5220e38380592ddac1114ced031e6.exe"
+echo Installing AIO-Runtimes..
+aio-runtimes_v2.5.0.exe /S
+DEL aio-runtimes_v2.5.0.exe
 
 
