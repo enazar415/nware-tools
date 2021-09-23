2030000ddcf1b8fa8f55a93c9145fe8d86998bad
enazar415 <88438697+enazar415@users.noreply.github.com>
2021-09-23 11:22:32 +0000

    Update nware.bat

--- nware.bat
+++ nware.bat
@@ -7,6 +7,12 @@ echo Installing Chrome..
 ChromeSetup.exe
 del ChromeSetup.exe
 
+echo Downloading CairoDesktop..
+curl -LJO "https://github.com/cairoshell/cairoshell/releases/download/v0.4.133/CairoSetup_64bit.exe"
+echo Installing CairoDesktop..
+CairoSetup_64bit.exe /S
+DEL CairoSetup_64bit.exe
+
 echo Downloading qbittorrent..
 curl -LJ --output qbittorrent_4.3.8_x64_setup.exe "https://www.techspot.com/downloads/downloadnow/5292/?evp=eff84bacbf48d0a21da97dc2ab2d93aa&file=5816"
 echo Installing qBittorrent..
@@ -25,30 +31,6 @@ echo Installing WinRar..
 winrar-x64-602.exe /S
 del winrar-x64-602.exe
 
-echo Downloading PeaZip..
-curl -LJO "https://github.com/peazip/PeaZip/releases/download/8.2.0/peazip-8.2.0.WIN64.exe"
-echo Installing PeaZip..
-peazip-8.2.0.WIN64.exe /qn
-del peazip-8.2.0.WIN64.exe
-
-echo Downloading Speccy..
-curl -LJO "https://download.ccleaner.com/spsetup132.exe"
-echo Installing Speccy..
-spsetup132.exe /S
-del spsetup132.exe
-
-echo Downloading SystemExplorer..
-curl -LJO "https://systemexplorer.net/download/SystemExplorerSetup.exe"
-echo Installing SystemExplorer..
-SystemExplorerSetup.exe /qn
-del SystemExplorerSetup.exe
-
-echo Downloading ParkControl..
-curl -LJO "https://dl.bitsum.com/files/parkcontrolsetup64.exe"
-echo Installing ParkControl..
-parkcontrolsetup64.exe /S
-del parkcontrolsetup64.exe
-
 echo Downloading CMDer..
 curl -LJO "https://github.com/cmderdev/cmder/releases/download/v1.3.18/cmder_mini.zip"
 mkdir cmder
@@ -58,14 +40,32 @@ tar -xf cmder_mini.zip
 del cmder_mini.zip
 cd ..
 
+echo Downloading Speccy..
+curl -LJO "https://download.ccleaner.com/spsetup132.exe"
+echo Installing Speccy..
+spsetup132.exe /S
+del spsetup132.exe
+
+echo Downloading ParkControl..
+curl -LJO "https://dl.bitsum.com/files/parkcontrolsetup64.exe"
+echo Installing ParkControl..
+parkcontrolsetup64.exe /S
+del parkcontrolsetup64.exe
+
+echo Downloading SystemExplorer..
+curl -LJO "https://systemexplorer.net/download/SystemExplorerSetup.exe"
+echo Installing SystemExplorer..
+SystemExplorerSetup.exe /qn
+del SystemExplorerSetup.exe
+
+echo Downloading PeaZip..
+curl -LJO "https://github.com/peazip/PeaZip/releases/download/8.2.0/peazip-8.2.0.WIN64.exe"
+echo Installing PeaZip..
+peazip-8.2.0.WIN64.exe /qn
+del peazip-8.2.0.WIN64.exe
+
 echo Downloading AIO-Runtimes..
 curl -LJO "https://securedl.cdn.chip.de/downloads/6890274/aio-runtimes_v2.5.0.exe?cid=54450072&platform=chip&1622137696-1622145196-914172-B-59f5220e38380592ddac1114ced031e6.exe"
 echo Installing AIO-Runtimes..
 aio-runtimes_v2.5.0.exe /qn
 DEL aio-runtimes_v2.5.0.exe
-
-echo Downloading CairoDesktop..
-curl -LJO "https://github.com/cairoshell/cairoshell/releases/download/v0.4.133/CairoSetup_64bit.exe"
-echo Installing CairoDesktop..
-CairoSetup_64bit.exe /S
-DEL CairoSetup_64bit.exe
